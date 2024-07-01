c47e48fe3e9741f52f546979f210451d3097bbdc
Le-Chat99 <bgkhiem291@gmail.com>
2024-07-01 16:09:20 +0700

    I maked a webpage

--- src/main.py
+++ src/main.py
@@ -6,15 +6,18 @@ from generatepage import *
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-    #print("Deleting public directory...")
-    #if os.path.exists(dir_path_public):
-    #    shutil.rmtree(dir_path_public)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    #print("Copying static files to public directory...")
-    #copy_files_recursive(dir_path_static, dir_path_public)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
     
-    generate_page('~/2web/content/index.md', '~/2web/template.html', '~/2web/public/index.html')
+    generate_page(os.path.join(dir_path_content, "index.md"),
+        template_path, os.path.join(dir_path_public, "index.html"))
 
 main()
