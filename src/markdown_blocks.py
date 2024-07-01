aece4740ae24f331523317265fe05f200124b7df
Le-Chat99 <bgkhiem291@gmail.com>
2024-07-01 14:19:33 +0700

    Ch1-4 patch

---
+++ src/markdown_blocks.py
@@ -0,0 +1,128 @@
+import re
+from textnode import *
+from htmlnode import *
+from inline_markdown import *
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
+
+
+def block_to_block_type(block):
+    heading_pattern= r"^#{1,6}\s+.*"
+    code_pattern= r"```[\s\S]*?```"
+    quote_pattern= r"(^>.*(?:\n>.*)*)"
+    unorder_pattern= r"^(\*|\-)\s+.*"
+    order_pattern= r"^\d+\.\s+.*"
+    if re.match(heading_pattern,block):
+        return block_type_heading
+    elif re.match(code_pattern,block,re.DOTALL):
+        return block_type_code
+    elif re.match(quote_pattern,block):
+        return block_type_quote
+    elif re.match(unorder_pattern,block):
+        return block_type_unordered_list
+    elif re.match(order_pattern,block):
+        return block_type_ordered_list
+    else: return block_type_paragraph
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+    
+def quote_to_html_node(block):
+    lines=block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("Invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
+def unorderedlist_to_html_node(block):
+    items=block.split("\n")
+    items_list=[]
+    for item in items:
+        text=item[2:]
+        children = text_to_children(text)
+        items_list.append(ParentNode("li",children))
+    return ParentNode("ul", items_list)
+
+def orderedlist_to_html_node(block):
+    items=block.split("\n")
+    items_list=[]
+    for item in items:
+        text=item[3:]
+        children = text_to_children(text)
+        items_list.append(ParentNode("li",children))
+    return ParentNode("ol", items_list)
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("Invalid code block")
+    text = block[4:-3]
+    children = text_to_children(text)
+    code = ParentNode("code", children)
+    return ParentNode("pre", [code])
+
+def heading_to_html_node(block):
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"Invalid heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+def block_to_html_node(block):
+    block_type=block_to_block_type(block)
+    if block_type==block_type_code:
+        return code_to_html_node(block)
+    if block_type==block_type_unordered_list:
+        return unorderedlist_to_html_node(block)
+    if block_type==block_type_ordered_list:
+        return orderedlist_to_html_node(block)
+    if block_type==block_type_heading:
+        return heading_to_html_node(block)
+    if block_type==block_type_quote:
+        return quote_to_html_node(block)
+    if block_type==block_type_paragraph:
+        return paragraph_to_html_node(block)
+    raise ValueError("Invalid block type")
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import *
+
 text_type_text = "text"
 text_type_bold = "bold"
 text_type_italic = "italic"
@@ -18,3 +20,20 @@ class TextNode:
         )
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode("b", text_node.text)
+        case "italic":
+            return LeafNode("i", text_node.text)
+        case "code":
+            return LeafNode("code", text_node.text)
+        case "link":
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"Invalid text type: {text_node.text_type}")
