1faf148106717390295e1689c35a9494c20ff39d
Le-Chat99 <bgkhiem291@gmail.com>
2024-07-01 16:00:16 +0700

    Why can't find index.md

--- src/main.py
+++ src/main.py
@@ -2,17 +2,19 @@ from textnode import TextNode
 import os
 import shutil
 from copystatic import *
+from generatepage import *
 
 dir_path_static = "./static"
 dir_path_public = "./public"
 
 def main():
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    #print("Deleting public directory...")
+    #if os.path.exists(dir_path_public):
+    #    shutil.rmtree(dir_path_public)
 
-    print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
-
+    #print("Copying static files to public directory...")
+    #copy_files_recursive(dir_path_static, dir_path_public)
+    
+    generate_page('~/2web/content/index.md', '~/2web/template.html', '~/2web/public/index.html')
 
 main()
