6d9f1e1805c0085be6675c4b975f7609b392f3fb
Le-Chat99 <bgkhiem291@gmail.com>
2024-07-02 07:26:33 +0700

    my first project: completed

--- src/generatepage.py
+++ src/generatepage.py
@@ -1,5 +1,6 @@
 from markdown_blocks import *
 import os
+from pathlib import Path
 
 def extract_title(markdown):
     lines =markdown.split("\n")
@@ -28,3 +29,15 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
+
+    
